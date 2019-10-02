76708b1c7ef0c90721053f8950bd6149e13c74c8
kaishwarya <kumari_aishwarya@intuit.com>
2019-10-02 14:20:08 +0530

    Adding custom format to input

---
+++ tool_pkg/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='base64tool',  
+     version='0.2',
+     scripts=['base64tool'] ,
+     author="Kumari Aishwarya",
+     author_email="k.aishwarya15@gmail.com ",
+     description="A tool to encode decode Base64",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/k-aishwarya/Base64Tool",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
